89c0373c627bdb31a06ffb7d99328002fb81b500
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-08-23 09:53:00 -0300

    feat: configurando o banco de dados

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RangoAgil.API.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RangoDbContext>(
+    o => o.UseSqlite(
+        builder.Configuration["ConnectionStrings.RangoDbConStr"]
+    
+    )
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
