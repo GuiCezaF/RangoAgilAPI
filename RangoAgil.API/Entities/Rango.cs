00483a02dfbd9926c818a4f63131c9ee716d9fd5
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-08-23 09:52:48 -0300

    feat: adicionando alguns decoratos nas entidades

--- RangoAgil.API/Entities/Rango.cs
+++ RangoAgil.API/Entities/Rango.cs
@@ -1,15 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
 namespace RangoAgil.API.Entities;
 
 public class Rango
 {
+    [Key]
     public int Id { get; set; }
-    public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public required string Name { get; set; }
+    
+    public ICollection<Ingrediente> Ingredientes { get; set; } = [];
+
 
     public Rango()
     {
         
     }
-
+    
+    [SetsRequiredMembers]
     public Rango(int id, string name)
     {
         Id = id;
