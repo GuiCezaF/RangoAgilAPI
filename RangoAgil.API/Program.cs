b0f4086f48b486c4b972bcf661a03c5b7e0e4556
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-08-27 09:44:54 -0300

    feat: criando primeiras rotas para retornar os rangos

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RangoAgil.API.DbContext;
+using RangoAgil.API.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,5 +15,26 @@ builder.Services.AddDbContext<RangoDbContext>(
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/rangos", async Task<Results<NoContent, Ok<List<Rango>>>>
+    (RangoDbContext rangoDbContext,
+    [FromQuery(Name = "name")] string? rangoNome) =>
+{
+    
+    var rangoEntity = await rangoDbContext.Rangos
+        .Where(r => rangoNome == null ||  r.Name.ToLower().Contains(rangoNome.ToLower()))
+        .ToListAsync();
+
+    if (rangoEntity.Count <= 0)
+        return TypedResults.NoContent();
+        
+    return TypedResults.Ok(rangoEntity);
+});
+
+app.MapGet("/rango/{id:int}",
+    async (RangoDbContext rangoDbContext, int id) =>
+    {
+        return await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == id);
+    });
 
 app.Run();
