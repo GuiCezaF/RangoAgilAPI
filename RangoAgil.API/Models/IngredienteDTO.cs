56ff9c53c46e627404560892ad7e6cf8910c0ecb
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-08-29 15:12:40 -0300

    feat: adicionando auto mapper e criando DTOs

---
+++ RangoAgil.API/Models/IngredienteDTO.cs
@@ -0,0 +1,10 @@
+namespace RangoAgil.API.Models;
+
+public class IngredienteDTO
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public int RangoId { get; set; }
+}
---
+++ RangoAgil.API/Models/RangoDTO.cs
@@ -0,0 +1,9 @@
+namespace RangoAgil.API.Models;
+
+public class RangoDTO
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+    
+}
